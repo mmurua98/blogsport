2fb09db6da58735cea4862921b5e16639c202aaf
mmurua98 <55469486+mmurua98@users.noreply.github.com>
2021-04-11 21:29:30 -0700

    change appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -26,6 +27,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-        URL::forceScheme('https');
+        //URL::forceScheme('https');
+        if (App::environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
